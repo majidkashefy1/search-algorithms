e52304302cf69fd1afba23a37f95b1a4e05cbc79
Majid\MajidK <kashefymajid1992@gmail.com>
2025-11-02 12:15:44 +0330

    add jump search

---
+++ algorithms/jump_search.py
@@ -0,0 +1,18 @@
+import math
+
+def jump_search(arr, target):
+    """Jump Search algorithm for sorted arrays."""
+    n = len(arr)
+    step = int(math.sqrt(n))
+    prev = 0
+
+    while prev < n and arr[min(step, n) - 1] < target:
+        prev = step
+        step += int(math.sqrt(n))
+        if prev >= n:
+            return -1
+
+    for i in range(prev, min(step, n)):
+        if arr[i] == target:
+            return i
+    return -1

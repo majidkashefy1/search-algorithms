a286e4409b98dbe2f35bc0d3f7b3d40927bbd5e6
Majid\MajidK <kashefymajid1992@gmail.com>
2025-11-02 12:11:27 +0330

    add bonary serach

---
+++ algorithms/binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(arr, target):
+    """Binary search on a sorted list."""
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1

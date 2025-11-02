f789d86c292a987aaf68d4a15201182e6e5351e6
Majid\MajidK <kashefymajid1992@gmail.com>
2025-11-02 12:14:01 +0330

    add exponential search

---
+++ algorithms/exponential_search.py
@@ -0,0 +1,14 @@
+from algorithms.binary_search import binary_search
+
+def exponential_search(arr, target):
+    """Exponential Search - finds the range, then uses binary search."""
+    if len(arr) == 0:
+        return -1
+    if arr[0] == target:
+        return 0
+
+    i = 1
+    while i < len(arr) and arr[i] <= target:
+        i *= 2
+
+    return binary_search(arr[:min(i, len(arr))], target)
